7bd50d0f4316daac6f328b3269130de5698ab733
Sebastian Yousef <sebyou@kth.se>
2026-09-04 17:17:17 +0200

    Release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.github.sebastianyousef.heed"
         minSdk = 26          // NotificationListenerService removal-reasons + channels
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.9.3"
+        versionCode = 19
+        versionName = "1.0.0"
     }
 
     signingConfigs {
